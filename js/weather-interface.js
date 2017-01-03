e7ce6bbf85b1d6e46d05564a8b920590493c497c
Daniel Munger and Ryan Peterson <daniel@>
2017-01-03 11:25:36 -0800

    refactored code to seperate front end from back end

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,11 +1,16 @@
 
 var Weather = require('./../js/weather.js').weatherModule;
 
+var displayTemp = function(city, tempData)
+{
+  $('.showWeather').text("The temperature in " + city + " is " + ((tempData * (9/5)) - 459) + " degrees Farenheit");
+};
+
 $(document).ready(function() {
   var currentWeatherObject = new Weather();
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    currentWeatherObject.getWeather(city);
+    currentWeatherObject.getWeather(city, displayTemp);
   });
 });
--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,16 @@
 var apiKey = require('./../.env').apiKey;
-function Weather(){
+
+function Weather()
+{
+
 }
 
-Weather.prototype.getWeather = function(city)
-{
-  console.log("hi");
+Weather.prototype.getWeather = function(city, displayFunction) {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-    $('.showWeather').text("The temperature in " + city + " is " + ((response.main.temp * (9/5)) - 459) + " degrees Farenheit");
+    displayFunction(city, response.main.temp);
   }).fail(function(error) {
     $('.showWeather').text(error.responseJSON.message);
   });
-}
+};
 
 exports.weatherModule = Weather;
